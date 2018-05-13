939057f025e94dbd90b84d8a219a184c1707357f
Shivanisingh05 <shivanisingh8990@gmail.com>
2018-05-14 03:10:01 +0530

    Twitterbot

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from twitter import Twitter, OAuth
 import tweepy
-import textblob
+import re
+from termcolor import colored
 from textblob import TextBlob
+from paralleldots import set_api_key, get_api_key, sentiment
+import nltk
+from nltk.corpus import *
+from nltk import Counter
+#-----------------------------------------------------------------------------------------------------------------------
+
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+print ("\033[35;40m*************************************************************************************************************************************************************************************")
+print (colored("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tWELCOME TO TWITTERBOT", color= 'red', attrs=['bold']))
+
 API_KEY = 'OZdisl10940KgoyOOiVXheGv5'
 API_SECRET = 'jWXndj1SMWMnP1ISvU0lj2fyt7tcEQUJV5O6ziyFB52HljqKJU'
 ACCESS_TOKEN = '990982189818044417-3V8KzMJEOhTR9b4PEzkUXpl4b9izCw2'
@@ -10,66 +23,261 @@ ACCESS_TOKEN_SECRET = 'ndU8rHLVbIE75GkrJz5PvVTuXXFsYerYEdoRSf0ePtdUF'
 twitter_oauth = OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, API_KEY, API_SECRET)
 twitter = Twitter(auth = twitter_oauth)
 
+oauth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+oauth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(oauth)
+#-----------------------------------------------------------------------------------------------------------------------
+
+#FOR DISPLAYING MY DETAILS
+def details():
+    user = api.me()
+    print (colored("\nMY DETAILS", color='red', attrs=['underline']))
+    print 'Name: '  + user.name
+    print 'Location: ' + user.location
+    print 'Friends: ' + str(user.friends_count)
+    print (colored("*****************************************************************************************"
+                   "********************************************************************************************",
+                   color='green'))
+#-----------------------------------------------------------------------------------------------------------------------
+
+# FOR GETTING QUERY
+
 def get_tweets(query):
-    tweets = twitter.search.tweets(q='#' +query, count=100)
+    tweets = twitter.search.tweets(q='#' +query, count=200)
     return tweets
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# FOR GETTING NUMBER OF FOLLOWERS
 
 def get_num_followers(query):
     num_followers = 0
     tweets = get_tweets(query)
     #print type(tweets)
     for each_tweet in tweets['statuses']:
-        print each_tweet['user']['followers_count']
+        print (colored("*****************************************************************************************"
+               "********************************************************************************************", color= 'green'))
+        print "Name: " + each_tweet['user']['screen_name']
+        print "Followers: " + str(each_tweet['user']['followers_count'])
+        # print each_tweet['user']['location']
         num_followers += each_tweet['user']['followers_count']
     return  num_followers
 
-def sentiments(query):
-    oauth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-    oauth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tweepy.API(oauth)
+#-----------------------------------------------------------------------------------------------------------------------
+
+# FOR GETTING SENTIMENTS
+
+def get_sentiments(query):
+    p = 0
+    n=0
+    ne = 0
+    set_api_key('kQmnPQVgdkzuRHaPpQJgqXLAmWlYV6GCx62z8LuitUQ')
+    get_api_key()
     public_tweets = api.search(query)
     for tweet in public_tweets:
-        print(tweet.text)
-        analyse = TextBlob(tweet.text)
-        print(analyse.sentiment)
-        print(analyse.detect_language())
-        print("######################################################################################################")
+        text = tweet.text
+        print (colored("*****************************************************************************************"
+                       "********************************************************************************************",
+                       color='green'))
+        print (colored(tweet.text, color='blue'))
+        r = sentiment(tweet.text)
+        print(colored(r, color= 'red'))
+        result = r['sentiment']
+        if result == "positive":
+            p = p+1
+        elif  r['sentiment'] == "neutral":
+            n = n+1
+        else:
+            ne = ne+1
+    print (colored("*****************************************************************************************"
+                   "********************************************************************************************",
+                   color='red'))
+    print "Maximum positive comments: ", p
+    print "Maximum neutral comments: ", n
+    print "Maximum negative comments: ", ne
+    print (colored("*****************************************************************************************"
+                   "********************************************************************************************",
+                   color='red'))
 
+#-----------------------------------------------------------------------------------------------------------------------
+
+# FOR GETTING LOCATION, LANGUAGE AND TIME ZONE
+
+def llt(query):
+
+    public_tweets = get_tweets(query)
+    lo = {}
+    la = {}
+    tz = {}
+    for tweet in public_tweets ['statuses']:
+
+        loc = [tweet['user']['location']]
+        lan = [tweet['user']['lang']]
+        tz1 =  tweet['user']['created_at']
+        # for f in loc:
+        #     if f not in lo:
+        #         loc.append(f)
+        # num = Counter(loc).most_common(2)
+        # print num
+        if loc in lo:
+            lo[loc] +=1
+        else:
+            lo[loc] = 1
+        if lan in la:
+            la[lan] += 1
+        else:
+            la[lan] = 1
+        if tz1 in tz:
+            tz[tz1] += 1
+        else:
+            tz[tz1] = 1
+        if tz == None or tz == "" or tz[tz1] == 1:
+            del tz[None]
+            del tz[""]
+            del tz[:][1]
+        if lo == None or lo == "" or lo[loc] == 1:
+            del lo[None]
+            del lo[""]
+            del lo[:][1]
+        if la == None or la == "" or la[lan] == 1:
+            del la[None]
+            del la[""]
+            del la[:][1]
+        print (colored("Time Zone: ", color='red', attrs=['bold']) + tweet['user']['created_at'])
+        print (colored("Language: ", color='red', attrs=['bold']) + tweet['user']['lang'])
+        print (colored("Location: ", color='red', attrs=['bold']) + tweet['user']['location'])
+        print (colored("*****************************************************************************************"
+                       "********************************************************************************************",
+                       color='green'))
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# FOR COMPARISION OF MODI AND TRUMP TWEETS
+
+def Comparision():
+
+    # FOR MODI
+    count = 0
+    new_tweets = api.user_timeline(screen_name = '@narendramodi', count = 200, tweet_mode='extended')
+    for tweet in new_tweets:
+        print(tweet.full_text)
+        text = tweet.full_text
+        temp = []
+        temp.append(text)
+        temp1 = temp
+        words = re.sub(r"http\S+", "", str(temp1))
+        word = words.split()
+        for i in word:
+            i = i.upper()
+            if i == "USA" or i == "US" or i == "America" or i == "United States of America":
+                count = count + 1
+    print (colored("*****************************************************************************************"
+                   "********************************************************************************************",
+                   color='red'))
+    print "Number of times Narendra Modi mentioned 'USA', 'US', 'America', or 'United States Of America: ", count
+
+
+    #FOR TRUMP
+    count1 = 0
+    new_tweets = api.user_timeline(screen_name='@realDonaldTrump', count=200, tweet_mode='extended')
+    for tweet in new_tweets:
+        print(tweet.full_text)
+        text = tweet.full_text
+        temp = []
+        temp.append(text)
+        temp1 = temp
+        words = re.sub(r"http\S+"," ", str(temp1))
+        word = words.split()
+        for i in word:
+            i = i.lower()
+            if i == "India" or i == "INDIA":
+                count1 = count1 + 1
+    print "Number of times Donald Trump mentioned 'India': ", count1
+    print (colored("*****************************************************************************************"
+                   "********************************************************************************************",
+                   color='red'))
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# FOR DETERMINING TOP USAGE STOPWORDS
+
+def Topusage():
+    new_tweets = api.user_timeline(screen_name='@narendramodi', count=200, tweet_mode='extended')
+    for tweet in new_tweets:
+        #print(tweet.full_text)
+        temp = []
+        temp.append(tweet.full_text)
+        temp1 = temp
+        import re
+        words = re.sub(r"http\S+"," ", str(temp1))
+        word = words.split()
+        word1 = [w for w in word if w in stop_words]
+        for w in word1:
+            if w not in stop_words:
+                word1.append(w)
+        num = Counter(word1).most_common(10)
+        #print word1
+        print(num)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# FOR UPDATING STATUS
+
+def tweet_status():
+    status = raw_input("Enter your new tweet: ")
+    api.update_status(status)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# MAIN FUNCTION
 
 def main():
+ print (colored("*****************************************************************************************"
+                   "********************************************************************************************",
+                   color='red'))
  while (True):
-    user_choice = input("What would you like to do? \n"
-                   "1. Count the number of people Tweeting using a certain hash tag.\n"
-                   "2. Determine the location, timezone and language of people Tweeting using a certain has tag.\n"
-                   "3. Number of times Modi has referred USAin the past 200 tweets compared to how many time Trumph has mentioned India.\n"
-                   "4. Determine the sentiments of people Tweeting using a certain has tag.\n"
-                   "5. Top used word by PM MODI on twitter.\n"
-                   "6. Tweet a message from your account.\n"
-                   "7. Exit\n")
 
+     user_choice = input("\nWhat would you like to do? \n"
+                   "1. Show my details.\n"
+                   "2. Count the number of followers. \n"
+                   "3. Determine the sentiments of people Tweeting using a certain has tag. \n"
+                   "4. Determining the location, timezone and language of people Tweeting using a certain has tag. \n"
+                   "5. Comparision of tweets by Narendera Modi and Donald Trump. \n"
+                   "6. Determining Top Usage. \n"
+                   "7. Tweet a message from your account. \n"
+                   "8. Exit\n"
+                   "User Choice: ")
 
-    if user_choice == 1:
+     if user_choice == 1:
+         details()
+
+     elif user_choice == 2:
+        print ("Chosen to count the number of followers.")
         user_input = raw_input("Enter the has tag: ")
-        print "\n\nTotal number of people who might have seen this has tag are: %s" % (get_num_followers(user_input))
-    elif user_choice == 2:
+        print "\n Maximum number of people who might have seen this has tag are: %s " % (get_num_followers(user_input))
+
+     elif user_choice == 3:
+         user_input = raw_input("Enter the has tag: ")
+         get_sentiments(user_input)
+
+     elif user_choice == 4:
         user_input = raw_input("Enter the has tag: ")
+        llt(user_input)
 
-    elif user_choice == 3:
-        pass
-        #user_input = raw_input("Enter the has tag.")
+     elif user_choice == 5:
+        Comparision()
 
-    elif user_choice == 4:
-        user_input = raw_input("Enter the has tag: ")
-        (sentiments(user_input))
-    elif user_choice == 5:
-       pass
-       #user_input = raw_input("Enter the has tag.")
+     elif user_choice == 6:
+          Topusage()
 
-    elif user_choice == 6:
-        user_input = raw_input("Enter the has tag.")
-    elif user_choice == 7:
+     elif user_choice == 7:
+
+         tweet_status()
+
+     elif user_choice == 8:
        break
-    else:
+     else:
         print("wrong choice try again.")
         exit()
 main()
